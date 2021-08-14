3646f93c146d81bce219b83d6743dff06accf4c6
KmolYuan <pyslvs@gmail.com>
2021-08-14 17:44:45 +0800

    Simplify TLBO.

--- src/methods/tlbo.rs
+++ src/methods/tlbo.rs
@@ -7,16 +7,13 @@ use ndarray::{s, Array1};
 pub type TLBOSetting = Setting;
 
 /// Teaching Learning Based Optimization type.
-pub struct TLBO<F: ObjFunc> {
-    tmp: Array1<f64>,
-    base: AlgorithmBase<F>,
-}
+pub struct TLBO<F: ObjFunc>(AlgorithmBase<F>);
 
 impl<F: ObjFunc> TLBO<F> {
-    fn register(&mut self, i: usize) {
-        let f_new = self.func.fitness(&self.tmp, &self.report);
+    fn register(&mut self, i: usize, student: &Array1<f64>) {
+        let f_new = self.func.fitness(student, &self.report);
         if f_new < self.fitness[i] {
-            self.base.pool.slice_mut(s![i, ..]).assign(&self.tmp);
+            self.pool.slice_mut(s![i, ..]).assign(student);
             self.fitness[i] = f_new;
         }
         if f_new < self.report.best_f {
@@ -24,7 +21,7 @@ impl<F: ObjFunc> TLBO<F> {
         }
     }
 
-    fn teaching(&mut self, i: usize) {
+    fn teaching(&mut self, i: usize, student: &mut Array1<f64>) {
         let tf = f64::round(rand() + 1.);
         for s in 0..self.dim {
             let mut mean = 0.;
@@ -34,12 +31,12 @@ impl<F: ObjFunc> TLBO<F> {
             mean /= self.dim as f64;
             let v =
                 self.pool[[i, s]] + rand_float(1., self.dim as f64) * (self.best[s] - tf * mean);
-            self.tmp[s] = self.check(s, v);
+            student[s] = self.check(s, v);
         }
-        self.register(i);
+        self.register(i, student);
     }
 
-    fn learning(&mut self, i: usize) {
+    fn learning(&mut self, i: usize, student: &mut Array1<f64>) {
         let j = {
             let j = rand_int(0, self.pop_num - 1);
             if j >= i {
@@ -55,15 +52,15 @@ impl<F: ObjFunc> TLBO<F> {
                 self.pool[[j, s]] - self.pool[[i, s]]
             };
             let v = self.pool[[i, s]] + rand_float(1., self.dim as f64) * diff;
-            self.tmp[s] = self.check(s, v);
+            student[s] = self.check(s, v);
         }
-        self.register(i);
+        self.register(i, student);
     }
 }
 
 impl<F: ObjFunc> DerefMut for TLBO<F> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.base
+        &mut self.0
     }
 }
 
@@ -71,7 +68,7 @@ impl<F: ObjFunc> Deref for TLBO<F> {
     type Target = AlgorithmBase<F>;
 
     fn deref(&self) -> &Self::Target {
-        &self.base
+        &self.0
     }
 }
 
@@ -79,18 +76,15 @@ impl<F: ObjFunc> Algorithm<F> for TLBO<F> {
     type Setting = TLBOSetting;
 
     fn create(func: F, settings: Self::Setting) -> Self {
-        let base = AlgorithmBase::new(func, settings);
-        Self {
-            tmp: Array1::zeros(base.dim),
-            base,
-        }
+        Self(AlgorithmBase::new(func, settings))
     }
 
     #[inline(always)]
     fn generation(&mut self) {
         for i in 0..self.pop_num {
-            self.teaching(i);
-            self.learning(i);
+            let mut student = Array1::zeros(self.dim);
+            self.teaching(i, &mut student);
+            self.learning(i, &mut student);
         }
     }
 }
