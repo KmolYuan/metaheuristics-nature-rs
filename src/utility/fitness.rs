542d1b651f8538f3abf308e364503eae8ac5b73d
KmolYuan <pyslvs@gmail.com>
2021-12-20 22:56:32 +0800

    Add example for fitness.

--- src/utility/fitness.rs
+++ src/utility/fitness.rs
@@ -3,5 +3,50 @@
 /// Usually, we can use numeric [`f64`] / [`f32`] type as the return value.
 /// More advanced, any type that implements the requirement trait can be used,
 /// so the fitness can add special mark during comparison.
+///
+/// In the following example, an "important" marker has higher priority in the comparison.
+///
+/// ```
+/// use metaheuristics_nature::{ObjFunc, utility::Fitness};
+/// use std::cmp::Ordering;
+/// # struct MyFunc([f64; 2], [f64; 2]);
+///
+/// #[derive(Clone, PartialEq, Default)]
+/// struct MarkerFitness {
+///     f: f64,
+///     important: bool,
+/// }
+///
+/// impl PartialOrd for MarkerFitness {
+///     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+///         match (self.important, other.important) {
+///             (false, false) | (true, true) => self.f.partial_cmp(&other.f),
+///             (true, false) => Some(Ordering::Greater),
+///             (false, true) => Some(Ordering::Less),
+///         }
+///     }
+/// }
+///
+/// impl ObjFunc for MyFunc {
+///     type Result = f64;
+///     type Fitness = MarkerFitness;
+///
+///     fn fitness(&self, v: &[f64], _: f64) -> Self::Fitness {
+///        MarkerFitness { f: v[0], important: v[0] + v[1] < 1. }
+///     }
+///
+///     fn result(&self, v: &[f64]) -> Self::Result {
+///         self.fitness(v, 0.).f
+///     }
+///
+///     fn ub(&self) -> &[f64] {
+///         &self.1
+///     }
+///
+///     fn lb(&self) -> &[f64] {
+///         &self.0
+///     }
+/// }
+/// ```
 pub trait Fitness: Sync + Send + Default + Clone + PartialOrd {}
 impl<T> Fitness for T where T: Sync + Send + Default + Clone + PartialOrd {}
