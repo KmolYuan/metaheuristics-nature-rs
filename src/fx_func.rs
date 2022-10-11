dbab9fa45c969005937f7ff16f5aedd57ac3aeb8
KmolYuan <pyslvs@gmail.com>
2022-10-11 16:39:22 +0800

    Remove macro in Fx type.

--- src/fx_func.rs
+++ src/fx_func.rs
@@ -1,62 +1,43 @@
 use crate::utility::prelude::*;
 use alloc::boxed::Box;
 
-macro_rules! impl_fx {
-    ($($(#[$meta:meta])* struct $ty:ident {
-        box($($func:tt)+),
-        fn($v:ident)($($expr:expr),+),
-    })+) => {$(
-        $(#[$meta])*
-        pub struct $ty<'f, 'b, R: Fitness, const DIM: usize> {
-            func: Box<dyn $($func)+ + Sync + Send + 'f>,
-            bound: &'b [[f64; 2]; DIM],
-        }
-
-        impl<'f, 'b, R: Fitness, const DIM: usize> $ty<'f, 'b, R, DIM> {
-            /// Create objective function from a callable object.
-            pub fn new<F>(bound: &'b [[f64; 2]; DIM], f: F) -> Self
-            where
-                F: $($func)+ + Sync + Send + 'f,
-            {
-                Self {
-                    func: Box::new(f),
-                    bound
-                }
-            }
-        }
-
-        impl<R: Fitness, const DIM: usize> Bounded for $ty<'_, '_, R, DIM> {
-            fn bound(&self) -> &[[f64; 2]] {
-                self.bound
-            }
-        }
-
-        impl<R: Fitness, const DIM: usize> ObjFunc for $ty<'_, '_, R, DIM> {
-            type Fitness = R;
-
-            fn fitness(&self, $v: &[f64]) -> Self::Fitness {
-                let $v = <[f64; DIM]>::try_from($v).unwrap();
-                (self.func)($($expr),+)
-            }
-        }
-    )+};
+/// A quick interface help to create objective function from a callable object.
+///
+/// ```
+/// use metaheuristics_nature::{Fx, Rga, Solver};
+///
+/// let bound = [[-50., 50.]; 4];
+/// let f = Fx::new(&bound, |[a, b, c, d]| a * a + 8. * b * b + c * c + d * d);
+/// let s = Solver::build(Rga::default())
+///     .task(|ctx| ctx.gen == 20)
+///     .solve(f)
+///     .unwrap();
+/// ```
+pub struct Fx<'f, 'b, R: Fitness, const DIM: usize> {
+    func: Box<dyn Fn([f64; DIM]) -> R + Sync + Send + 'f>,
+    bound: &'b [[f64; 2]; DIM],
 }
 
-impl_fx! {
-    /// A quick interface help to create objective function from a callable object.
-    ///
-    /// ```
-    /// use metaheuristics_nature::{Fx, Rga, Solver};
-    ///
-    /// let bound = [[-50., 50.]; 4];
-    /// let f = Fx::new(&bound, |[a, b, c, d]| a * a + 8. * b * b + c * c + d * d);
-    /// let s = Solver::build(Rga::default())
-    ///     .task(|ctx| ctx.gen == 20)
-    ///     .solve(f)
-    ///     .unwrap();
-    /// ```
-    struct Fx {
-        box(Fn([f64; DIM]) -> R),
-        fn(v)(v),
+impl<'f, 'b, R: Fitness, const DIM: usize> Fx<'f, 'b, R, DIM> {
+    /// Create objective function from a callable object.
+    pub fn new<F>(bound: &'b [[f64; 2]; DIM], func: F) -> Self
+    where
+        F: Fn([f64; DIM]) -> R + Sync + Send + 'f,
+    {
+        Self { bound, func: Box::new(func) }
+    }
+}
+
+impl<R: Fitness, const DIM: usize> Bounded for Fx<'_, '_, R, DIM> {
+    fn bound(&self) -> &[[f64; 2]] {
+        self.bound
+    }
+}
+
+impl<R: Fitness, const DIM: usize> ObjFunc for Fx<'_, '_, R, DIM> {
+    type Fitness = R;
+
+    fn fitness(&self, xs: &[f64]) -> Self::Fitness {
+        (self.func)(xs.try_into().unwrap())
     }
 }
