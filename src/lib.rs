77fe99fdcfb5eb568314ef81d8185fa452268a54
KmolYuan <pyslvs@gmail.com>
2022-10-30 13:48:57 +0800

    Rename module.

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@ extern crate alloc;
 #[cfg(not(feature = "std"))]
 extern crate core as std;
 
-pub use self::{algorithm::*, fx_func::*, methods::*, obj_func::*, setting::*, solver::*};
+pub use self::{algorithm::*, fx_func::*, methods::*, obj::*, setting::*, solver::*};
 
 /// A tool macro used to generate multiple builder functions (methods).
 ///
@@ -128,7 +128,7 @@ macro_rules! impl_builders {
 mod algorithm;
 mod fx_func;
 pub mod methods;
-mod obj_func;
+mod obj;
 mod setting;
 mod solver;
 pub mod tests;
