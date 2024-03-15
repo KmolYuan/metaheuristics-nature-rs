b45fd409464a225b0cfe5fa660a8e3c381b3e478
KmolYuan <pyslvs@gmail.com>
2024-03-15 11:41:18 +0800

    Add xs in the Pareto container

--- src/pareto.rs
+++ src/pareto.rs
@@ -23,24 +23,60 @@ use alloc::vec::Vec;
 pub trait Dominance {
     /// Check if `self` dominates `rhs`.
     fn is_dominated(&self, rhs: &Self) -> bool;
+    /// Evaluate the final fitness value.
+    fn eval(&self) -> f64;
 }
 
-impl<T: PartialOrd> Dominance for T {
+impl Dominance for f64 {
     fn is_dominated(&self, rhs: &Self) -> bool {
         self <= rhs
+    }
+    fn eval(&self) -> f64 {
+        *self
+    }
+}
+impl Dominance for f32 {
+    fn is_dominated(&self, rhs: &Self) -> bool {
+        self <= rhs
+    }
+    fn eval(&self) -> f64 {
+        *self as f64
     }
 }
 
 /// Single best element container.
 #[derive(Debug, Default)]
 pub struct SingleBest<T: Dominance> {
-    best: Option<T>,
+    xs: Option<Vec<f64>>,
+    fit: Option<T>,
 }
 
 /// Pareto front container.
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Pareto<T: Dominance> {
-    list: Vec<T>,
+    xs: Vec<Vec<f64>>,
+    fit: Vec<T>,
+    limit: usize,
+}
+
+impl<T: Dominance> Default for Pareto<T> {
+    fn default() -> Self {
+        Self::new(10)
+    }
+}
+
+impl<T: Dominance> Pareto<T> {
+    /// Create a new Pareto front container.
+    pub const fn new(limit: usize) -> Self {
+        Self { xs: Vec::new(), fit: Vec::new(), limit }
+    }
+
+    /// Set the limit of the Pareto front.
+    ///
+    /// This method will not change the solution set until the next update.
+    pub fn set_limit(&mut self, limit: usize) {
+        self.limit = limit;
+    }
 }
 
 /// A trait for best element container.
@@ -48,46 +84,61 @@ pub trait Best {
     /// The type of the best element
     type Item: Dominance;
     /// Update the best element.
-    fn update(&mut self, x: Self::Item);
+    fn update(&mut self, xs: Vec<f64>, fit: Self::Item);
     /// Sample a random best element.
-    fn sample(&self, rng: &Rng) -> Option<&Self::Item>;
+    fn sample(&self, rng: &Rng) -> Option<(&[f64], &Self::Item)>;
 }
 
 impl<T: Dominance> Best for SingleBest<T> {
     type Item = T;
 
-    fn update(&mut self, x: Self::Item) {
-        if let Some(best) = &mut self.best {
-            if x.is_dominated(best) {
-                *best = x;
+    fn update(&mut self, xs: Vec<f64>, fit: Self::Item) {
+        if let (Some(best), Some(best_f)) = (&mut self.xs, &mut self.fit) {
+            if fit.is_dominated(best_f) {
+                *best = xs;
+                *best_f = fit;
             }
         } else {
-            self.best = Some(x);
+            self.xs = Some(xs);
+            self.fit = Some(fit);
         }
     }
 
-    fn sample(&self, _rng: &Rng) -> Option<&Self::Item> {
-        self.best.as_ref()
+    fn sample(&self, _rng: &Rng) -> Option<(&[f64], &Self::Item)> {
+        self.xs.as_deref().zip(self.fit.as_ref())
     }
 }
 
 impl<T: Dominance> Best for Pareto<T> {
     type Item = T;
 
-    fn update(&mut self, x: Self::Item) {
+    fn update(&mut self, xs: Vec<f64>, fit: Self::Item) {
         let mut is_dominated = false;
-        self.list.retain(|y| {
-            let dominated = x.is_dominated(y);
+        // Remove dominated solutions
+        for i in 0..self.xs.len() {
+            let dominated = fit.is_dominated(&self.fit[i]);
             is_dominated |= dominated;
-            !dominated
-        });
+            if dominated {
+                self.xs.swap_remove(i);
+                self.fit.swap_remove(i);
+            }
+        }
         if is_dominated {
-            self.list.push(x);
+            self.xs.push(xs);
+            self.fit.push(fit);
+        }
+        if self.xs.len() > self.limit {
+            // Remove the oldest solution
+            let (i, _) = (self.fit.iter().map(T::eval).enumerate())
+                .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+                .unwrap();
+            self.xs.remove(i);
+            self.fit.remove(i);
         }
     }
 
-    fn sample(&self, rng: &Rng) -> Option<&Self::Item> {
-        use rand::seq::SliceRandom as _;
-        rng.gen(|r| self.list.choose(r))
+    fn sample(&self, rng: &Rng) -> Option<(&[f64], &Self::Item)> {
+        let i = rng.range(0..self.xs.len());
+        Some((self.xs.get(i)?, self.fit.get(i)?))
     }
 }
