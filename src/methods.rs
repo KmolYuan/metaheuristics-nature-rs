890ad9fbef12096faf02a00cd30dbbb8c6102c95
KmolYuan <pyslvs@gmail.com>
2024-03-18 15:51:29 +0800

    Improve pub imports

--- src/methods.rs
+++ src/methods.rs
@@ -2,7 +2,13 @@
 //!
 //! Each methods are also has some variants on implementation,
 //! current methods are just designed for application.
-pub use self::{de::De, fa::Fa, pso::Pso, rga::Rga, tlbo::Tlbo};
+pub use self::{
+    de::{De, Strategy},
+    fa::Fa,
+    pso::Pso,
+    rga::Rga,
+    tlbo::Tlbo,
+};
 
 pub mod de;
 pub mod fa;
